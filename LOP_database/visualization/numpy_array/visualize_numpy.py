3ba6d9f855f07bc574b1d894f4a16f69cd9db271
Leo Crestel Mac <crestel.leopold@gmail.com>
2017-11-15 13:48:36 +0900

    Create new directory if didn't exist

--- LOP_database/visualization/numpy_array/visualize_numpy.py
+++ LOP_database/visualization/numpy_array/visualize_numpy.py
@@ -2,13 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import os
 from LOP_database.utils.pianoroll_processing import sum_along_instru_dim
 from LOP_database.visualization.numpy_array.write_numpy_array_html import write_numpy_array_html
 from LOP_database.visualization.numpy_array.dumped_numpy_to_csv import dump_to_csv
 
 
 def visualize_dict(pr, path, file_name_no_extension, time_indices=None):
+    if not os.path.exists(path):
+        os.makedirs(path)
     AAA = sum_along_instru_dim(pr)
+    if time_indices:
+        start_time, end_time = time_indices
+        AAA = AAA[start_time:end_time, :]
+    temp_csv = path + '/' + file_name_no_extension + '.csv'
+    np.savetxt(temp_csv, AAA, delimiter=',')
+    dump_to_csv(temp_csv, temp_csv)
+    write_numpy_array_html(path + '/' + file_name_no_extension + ".html", file_name_no_extension, d3js_source_path='/Users/leo/Recherche/GitHub_Aciditeam/acidano/acidano/visualization/d3.v3.min.js')
+
+def visualize_dict_concat(pr, path, file_name_no_extension, time_indices=None):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    AAA = np.concatenate(pr.values(), axis=1)
     if time_indices:
         start_time, end_time = time_indices
         AAA = AAA[start_time:end_time, :]
@@ -19,6 +34,8 @@ def visualize_dict(pr, path, file_name_no_extension, time_indices=None):
 
 
 def visualize_mat(pr, path, file_name_no_extension, time_indices=None):
+    if not os.path.exists(path):
+        os.makedirs(path)
     if time_indices:
         start_time, end_time = time_indices
         pr = pr[start_time:end_time, :]
@@ -29,6 +46,8 @@ def visualize_mat(pr, path, file_name_no_extension, time_indices=None):
 
 
 def visualize_mat_proba(pr, path, file_name_no_extension, time_indices=None):
+    if not os.path.exists(path):
+        os.makedirs(path)    
     if time_indices:
         start_time, end_time = time_indices
         pr = pr[start_time:end_time, :]
